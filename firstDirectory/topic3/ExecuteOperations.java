0945a9d13f28efb215fa9f7bf2f28e4f5d7555e0
sdmnc <sergedominik@gmail.com>
2023-02-26 15:57:28 +0200

    Operations Branch

---
+++ firstDirectory/topic3/ExecuteOperations.java
@@ -0,0 +1,13 @@
+package firstDirectory.topic3;
+
+import static firstDirectory.topic3.Operations.*;
+
+public class ExecuteOperations {
+    public static void main(String [] args){
+        printArithmeticOperations();
+        printLogicOperations();
+        printRelationalOperation();
+        useForAndWhile();
+    }
+
+}
---
+++ firstDirectory/topic3/Operations.java
@@ -0,0 +1,95 @@
+package firstDirectory.topic3;
+
+public class Operations {
+    static void printArithmeticOperations() {
+        System.out.println("ArithmeticOperations");
+        int a = 10;
+        int b = 110;
+        int sum = a+b;
+        int min = b-a;
+        int der = a*b;
+        double quotient = b/a;
+        System.out.println("The sum of a+b equals " + sum);
+        System.out.println("B-A equals " + min);
+        System.out.println("The derivative  of a*b equals " + der);
+        System.out.println("The quotient  of b/ equals " + quotient);
+
+
+
+    }
+
+    static void printLogicOperations() {
+        System.out.println("Logic Operations");
+        // && (logical AND)
+       // || (logical OR)
+        // ! (logical NOT):
+        boolean a = true;
+        boolean b = false;
+
+// logical AND
+        if (a && b) {
+            System.out.println("This will not be printed.");
+        }
+
+// logical OR
+        if (a || b) {
+            System.out.println("This will be printed.");
+        }
+
+// logical NOT
+        if (!b) {
+            System.out.println("This will be printed.");
+        }
+
+
+    }
+
+    static void printRelationalOperation() {
+        System.out.println("Relational");
+        int a = 5;
+        int b = 7;
+
+// equal to
+        if (a == b) {
+            System.out.println("This will not be printed.");
+        }
+
+// not equal to
+        if (a != b) {
+            System.out.println("This will be printed.");
+        }
+
+// greater than
+        if (b > a) {
+            System.out.println("This will be printed.");
+        }
+
+// less than
+        if (a < b) {
+            System.out.println("This will be printed.");
+        }
+
+// greater than or equal to
+        if (b >= a) {
+            System.out.println("This will be printed.");
+        }
+
+// less than or equal to
+        if (a <= b) {
+            System.out.println("This will be printed.");
+        }
+
+    }
+    static void useForAndWhile(){
+        System.out.println("Cycles");
+        for(int i= 0; i<5; i++){
+            System.out.println(i);
+        }
+        int a =0;
+        while(a<4){
+            System.out.println(a);
+            a++;
+        }
+
+    }
+}
