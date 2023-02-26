1c98502685f6a0024be9680e27bd3d77b129e49e
sdmnc <sergedominik@gmail.com>
2023-02-26 15:35:06 +0200

    Variables

---
+++ topic2/Person.java
@@ -0,0 +1,25 @@
+package topic2;
+
+public class Person {
+   public String fName;
+  private  String name;
+    protected int age;
+     public String sex;
+    private int weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
