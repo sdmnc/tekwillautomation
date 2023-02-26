8a4e9fd389c8190ddde742bf619bc10b8c7b88ad
note <sergedominik@gmail.com>
2023-02-26 14:55:50 +0200

    Inheritance

---
+++ firstDirectory/Inheritance/Car.java
@@ -0,0 +1,12 @@
+package firstDirectory.Inheritance;
+
+public class Car extends Vehicle {
+    String carBrand = "Dacia";
+
+
+    public void showCarBrand() {
+        System.out.println(carBrand);
+
+    }
+}
+
