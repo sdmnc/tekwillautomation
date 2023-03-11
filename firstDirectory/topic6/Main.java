79b6b9b54623e4e0411360849221f5006c09a09f
sdmnc <sergedominik@gmail.com>
2023-03-11 11:17:42 +0200

    Sarcina Practica Exceptie

---
+++ firstDirectory/topic6/Main.java
@@ -0,0 +1,12 @@
+package firstDirectory.topic6;
+
+public class Main {
+    public static void main(String[] args) {
+        ContulPersoanei cont = new ContulPersoanei("John", 100, 500);
+        //cont.initialBalanceSet(300);
+        cont.sustragereDeBani(5.0);
+        cont.transferCatreOPersoana(100);
+    }
+
+
+}
