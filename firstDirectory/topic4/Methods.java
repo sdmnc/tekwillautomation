be4c1de21e9051002b0615c78e868b418d979e65
sdmnc <sergedominik@gmail.com>
2023-03-11 10:31:22 +0200

    Sarcina Practica Metode in Java

---
+++ firstDirectory/topic4/Methods.java
@@ -0,0 +1,11 @@
+package firstDirectory.topic4;
+
+
+public class Methods {
+    public static void main(String[] args) {
+        ExecuteMethods.sum(2, 10);
+
+        ExecuteMethods.sum(-11);
+    }
+
+}
