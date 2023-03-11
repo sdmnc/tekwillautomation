ccfb4801e1f3c4bd8bba6581760d9e16c906ffac
sdmnc <sergedominik@gmail.com>
2023-03-11 09:48:50 +0200

    Fixies per review3

--- firstDirectory/Inheritance/Car.java
+++ firstDirectory/Inheritance/Car.java
@@ -1,8 +1,19 @@
 package firstDirectory.Inheritance;
 
 public class Car extends Vehicle {
-    String carBrand = "Dacia";
+   private String carBrand = "Dacia";
 
+    public Car(String color, int numberOfWheels) {
+        super(color, numberOfWheels);
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
 
     public void showCarBrand() {
         System.out.println(carBrand);
--- firstDirectory/Inheritance/Vehicle.java
+++ firstDirectory/Inheritance/Vehicle.java
@@ -1,9 +1,15 @@
 package firstDirectory.Inheritance;
 
 public class Vehicle {
+    private String color;
+    private int numberOfWheels;
 
-    String color = "blue";
-    int numberOfWheels = 4;
+    public Vehicle(String color, int numberOfWheels) {
+        this.color = color;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    ;
 
 
     public void showCarDetails() {
@@ -13,7 +19,7 @@ public class Vehicle {
     }
 
     public static void main(String[] args) {
-        Car myCar = new Car();
+        Car myCar = new Car("red", 4);
         myCar.showCarDetails();
         myCar.showCarBrand();
 
