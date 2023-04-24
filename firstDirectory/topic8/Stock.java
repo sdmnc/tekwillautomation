416fa33f873c4121a5307d0ffb7aca91697a208e
sdmnc <sergedominik@gmail.com>
2023-04-24 15:04:15 +0300

    Books and Arrays

--- firstDirectory/topic8/Stock.java
+++ firstDirectory/topic8/Stock.java
@@ -1,2 +1,35 @@
-package firstDirectory.topic8;public class Stock {
+package firstDirectory.topic8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+    private int stockId;
+    private String adresaMagazin;
+
+    private List<Book> stoc = new ArrayList<>();
+
+
+    public void adaugaCarteaStock(Book book) {
+        stoc.add(book);
+
+    }
+
+    public void stergereaCarteaDinStoc(Book book) {
+        stoc.remove(book);
+
+    }
+
+    public int nrDeCartiInStoc() {
+        int number = stoc.size();
+        System.out.println(number);
+        return number;
+    }
+
+    public Boolean esteStoculGol() {
+        boolean stocGol = stoc.isEmpty();
+        System.out.println("The stock is empty:" + stocGol);
+
+        return stocGol;
+    }
 }
