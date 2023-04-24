9a5778beb8650fccc120c6a715dc60c6ec4b9c9a
sdmnc <sergedominik@gmail.com>
2023-04-24 13:42:31 +0300

    Adding try-catch as per code review

--- firstDirectory/topic6/Main.java
+++ firstDirectory/topic6/Main.java
@@ -2,11 +2,13 @@ package firstDirectory.topic6;
 
 public class Main {
     public static void main(String[] args) {
-        ContulPersoanei cont = new ContulPersoanei("John", 100, 500);
-        //cont.initialBalanceSet(300);
-        cont.sustragereDeBani(5.0);
-        cont.transferCatreOPersoana(100);
+        try {
+            ContulPersoanei cont = new ContulPersoanei("John", 100, 500);
+            cont.sustragereDeBani(5.0);
+            cont.transferCatreOPersoana(100);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+
+        }
     }
-
-
 }
